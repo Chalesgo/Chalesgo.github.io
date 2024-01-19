dd921ad10de871df2ddd387397ba192221242c30
Chalesgo <charleskristoffer.go@gmail.com>
2024-01-20 04:02:09 +0800

    finishing touches

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function toggleNightMode() {
+    document.body.classList.toggle('night-mode');
+}
+
+const nightModeButton = document.getElementById('night-mode-button');
+nightModeButton.addEventListener('click', toggleNightMode);
